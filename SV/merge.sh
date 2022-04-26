ef1fedc97823641f62411b23f52c544e8d2dd7d7
yilunhuangyue <really1105202@live.com>
2022-04-26 22:30:23 +0800

    Create merge.sh

---
+++ SV/merge.sh
@@ -0,0 +1,3 @@
+SURVIVOR merge list 200 1 1 -1 -1 -1 merged_200.vcf
+sniffles -m map.sort.bam -v spe_gt.vcf --Ivcf merged_200.vcf #genotype for each species by using mapping results of long reads
+SURVIVOR merge list_gt 200 -1 1 -1 -1 -1 merged_gt_200.vcf
