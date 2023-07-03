7662e949f0988d8acd81e3dd1520df972c57e57b
yilunhuangyue <really1105202@live.com>
2023-07-03 19:48:41 +0800

    Update raxml.sh

--- phylogenetic_analysis/raxml.sh
+++ phylogenetic_analysis/raxml.sh
@@ -1,1 +1,5 @@
-
+perl select_snp.pl 4dsite.txt all.vcf 4dsite.vcf # extracted SNPs at four-fold degenerate sites
+vcf2phylip.py -i 4dsite.vcf  # transfer the vcf format to phy format
+modeltest-ng -i 4dsite.min4.phy -d nt -p 10  # test for the best model
+raxml-ng  --bs-trees 1000 --model TVM+G4 --outgroup huajiao -msa 4dsite.min4.phy  --thread 20  --all --prefix tree  #construct phylogenetic trees
+r8s -b -f r8s_in.txt > r8s_out.txt  #estimated divergence time 
